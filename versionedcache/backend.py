aa4f83532ebbedfd9b56548c327a653ff8915aff
Honza Kral <honza.kral@gmail.com>
2011-07-13 16:06:38 +0200

    add **kwargs to be forwards compatible with Django 1.3

--- versionedcache/backend.py
+++ versionedcache/backend.py
@@ -38,15 +38,15 @@ class VersionHerdMixin(object):
         return value
 
 
-    def add(self, key, value, timeout=0):
+    def add(self, key, value, timeout=0, **kwargs):
         if isinstance(value, unicode):
             value = value.encode('utf-8')
-        return super(VersionHerdMixin, self).add(self._tag_key(key), *self._tag_value(value, timeout))
+        return super(VersionHerdMixin, self).add(self._tag_key(key), *self._tag_value(value, timeout), **kwargs)
 
-    def get(self, key, default=None):
+    def get(self, key, default=None, **kwargs):
         key = self._tag_key(key)
 
-        val = super(VersionHerdMixin, self).get(key, EXPIRED)
+        val = super(VersionHerdMixin, self).get(key, EXPIRED, **kwargs)
 
         # value not in cache
         if val is EXPIRED:
@@ -65,34 +65,33 @@ class VersionHerdMixin(object):
         else:
             return val
 
-    def set(self, key, value, timeout=0):
+    def set(self, key, value, timeout=0, **kwargs):
         if isinstance(value, unicode):
             value = smart_str(value)
-        super(VersionHerdMixin, self).set(self._tag_key(key), *self._tag_value(value, timeout))
+        super(VersionHerdMixin, self).set(self._tag_key(key), *self._tag_value(value, timeout), **kwargs)
 
     def delete(self, key, *args, **kwargs):
         super(VersionHerdMixin, self).delete(self._tag_key(key), *args, **kwargs)
 
-
-    def set_many(self, data, timeout=0):
+    def set_many(self, data, timeout=0, **kwargs):
         new_data = dict((self._tag_key(k), self._tag_value(v, timeout)[0]) for k, v in data.items())
-        return super(VersionHerdMixin, self).set_many(new_data, timeout)
+        return super(VersionHerdMixin, self).set_many(new_data, timeout, **kwargs)
 
-    def get_many(self, keys):
+    def get_many(self, keys, **kwargs):
         key_map = dict((self._tag_key(k), k) for k in keys)
         out = {}
-        for k, v in super(VersionHerdMixin, self).get_many(key_map.keys()).items():
+        for k, v in super(VersionHerdMixin, self).get_many(key_map.keys(), **kwargs).items():
             v = self._check_herd_protection(k, *v)
             if v is EXPIRED:
                 continue
             out[key_map[k]] = v
         return out
 
-    def incr(self, key, delta=1):
-        return base.BaseCache.incr(self, key, delta)
+    def incr(self, key, delta=1, **kwargs):
+        return base.BaseCache.incr(self, key, delta, **kwargs)
 
-    def decr(self, key, delta=1):
-        return base.BaseCache.decr(self, key, delta)
+    def decr(self, key, delta=1, **kwargs):
+        return base.BaseCache.decr(self, key, delta, **kwargs)
 
 class CacheClass(VersionHerdMixin, memcached.CacheClass):
     pass
